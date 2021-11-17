013038db008b1ac36283e6475eb60acb0067cdb1
onsummer <onsummer@foxmail.com>
2021-11-18 04:37:47 +0800

    [update] cesium vite react ts tpl CESIUM_BASE_URL typing.

---
+++ cesium-vite-reactts-tpl/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import CesiumScene from './CesiumScene'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CesiumScene />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
